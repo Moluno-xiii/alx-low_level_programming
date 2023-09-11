24f0b6d36bf1c5509e9d7f7999f5e77b0ac7534c
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-11 03:25:19 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers of base 10 starting with 0
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a =0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+}
