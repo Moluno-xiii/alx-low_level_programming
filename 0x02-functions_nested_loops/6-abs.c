d315b509a319a85ef24c7e439b8ac1757362399c
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-12 13:53:44 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs -Pprints absolute value of a number.
+ * @num: number to be printed
+ * Return: absolute value of number.
+ */
+
+int _abs(int num)
+{
+	if (num >= 0)
+	{
+		return (num);
+	}else
+	{
+		return (-num);
+	}
+
+	return (num);
+}
