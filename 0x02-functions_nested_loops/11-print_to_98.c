010eff95de937fc00931d7080ae9fc0482dfb0e7
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-12 15:10:07 -0700

    task 11 done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_to_98 - Prints all numbers to 98
+ * @num: starting point.
+ * Return: Always 0 (success)
+ */
 
+void print_to_98(int num)
+{
+	if (num < 98)
+	{
+		for (num = num; num < 98; num++)
+		{
+			printf("%d, ", num);
+		}
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (num = num; num > 98; num--)
+		{
+			printf("%d, ", num);
+		}
+		printf("%d\n", 98);
+	}
+}
