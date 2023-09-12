6f21ed18f14bb6bb04723afe11ba8cf734c97218
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-12 14:50:05 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+ /**
+  * add: Adds up two numbers
+  * @num1: first number
+  * @num2: second number
+  * Return: Always 0 (success)
+  */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
