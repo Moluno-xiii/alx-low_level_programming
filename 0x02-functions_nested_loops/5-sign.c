038f09140bee19051a559a4ca411e9f2f31efc4a
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-12 13:42:54 -0700

    task 5 done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,26 @@
 #include "main.h"
+/**
+ * print_sign - Prints the sign of number
+ * @number: integer to check
+ * Return: 1 if number is positive, 0 if number is zero and -1 if number is negative
+ */
+
+int print_sign(int number)
+{
+	if (number > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (number < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	else if (number == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+}
