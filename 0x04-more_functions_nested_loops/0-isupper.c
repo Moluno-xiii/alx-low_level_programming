c03d73b5e7b59ddbc70dfcac5a51cc2f442d0b59
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-14 07:41:47 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * _isupper - Checks if a lette is uppercase
+ * @c: letter to check
+ * Return: 1 if letter is uppercase and 0 if otherwise
+ */
+
+int _isupper(int letter)
+{
+	if (letter >= 65 && letter <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
