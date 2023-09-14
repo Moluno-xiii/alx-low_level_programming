28057e0996ac13cf687eb2875b1eae400cae6161
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-14 08:15:18 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers 0 to 9 except 2 and 4
+ * Return: Always 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		if (i == 0 || i == 2)
+		{
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
