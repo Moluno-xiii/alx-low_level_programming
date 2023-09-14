77d0e62f507483715eb82f20b4a446e21cddca99
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-14 08:09:26 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Multiple of the two integers
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
