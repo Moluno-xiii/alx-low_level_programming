149a3d55c23de95164ba80b69167a45b2d215501
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-14 08:27:40 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a line using dashes
+ * @n: number of dashes to use to print line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int a = 1;
+
+	while (a <= n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+		a++;
+	}
+	_putchar('\n');
+}
