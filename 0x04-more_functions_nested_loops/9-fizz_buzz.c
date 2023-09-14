65388e319adf15bafc96389ffcf5f83b2ee8d53d
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-14 09:16:09 -0700

    task 9 final

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 1 to 100 replacing multiples of 3 with
- * Fizz, multiples of 5 with Buzz, and multiples of both with FizzBuzz
- *
+ * main - Fizz buzz problem 
+ * 
  * Return: Always 0 (success)
  */
 
+
 int main(void)
 {
-    int number;
+	int number;
 
-    for (number = 1; number <= 100; number++)
-    {
-        if (number % 3 == 0 && number % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (number % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (number % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", number);
-        }
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (number % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", number);
+		}
 
-        if (number < 100)
-        {
-            printf(" ");
-        }
-    }
+		if (number < 100)
+		{
+			printf(" ");
+		}
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
-
