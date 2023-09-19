dd89f0c03ead6e2ada9f43a21f91ee5ed53127f0
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-18 19:26:13 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts2 - Function characters from a string
+ * @str: string
+ *
+ * Return: Always 0 (success)
+ */
+
+void puts2(char *str)
+{
+	int a, b, n;
+
+	a = 0;
+	if (str[a] != '\0')
+	{
+		_putchar(str[0]);
+
+		b = 0;
+		while (str[b] != '\0')
+		{
+			b++;
+		}
+
+		for (n - 1; n < 1; n++)
+		{
+			if (n % 2 == 0)
+			{
+				_putchar(str[n]);
+			}
+		}
+	}
+	else
+	{
+	}
+	_putchar('\n');
+}
