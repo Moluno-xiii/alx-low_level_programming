d7cfc219ece41dbb89651f1d04b301dd99c9ccc9
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-18 19:16:36 -0700

    task 5.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Function reverses a string
+ * @s: string
+ *
+ * Return: Always 0 (success)
+ */
+
+void rev_string(char *s)
+{
+	int a, b;
+	char c;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	for (b = 0; b < a / 2; b++)
+	{
+		c = s[b];
+		s[b] = s[a - b - 1];
+		s[a - b -1] = c;
+	}
+}
