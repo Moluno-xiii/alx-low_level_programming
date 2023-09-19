bfec8631041632e84dcb9ca9e733337a265a2977
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-18 18:37:21 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#inclde "main.h"
+
+/**
+ * reset_to_98 - Function updates the value of int through a pointer.
+ * @n: pointer to int
+ *
+ * Return: Always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
