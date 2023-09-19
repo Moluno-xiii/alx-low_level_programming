4a5c1ea1ce882426b0d9b49aa4b069ca17611fbc
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-18 18:44:42 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Function swaps the value of two integers
+ * @a: first pointer
+ * @b: second pointer
+ */
+
+void swap_int(int *a, int*b)
+{
+	int c = *a;
+	int d =*b;
+	*a = d;
+	*b = c;
+}
