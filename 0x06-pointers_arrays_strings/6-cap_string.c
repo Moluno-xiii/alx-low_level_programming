8150584fa905c904b10edacbdcb0cf471050196f
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-21 15:22:54 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - Function that capitalizes a string
+ * @str: string
+ *
+ * Return: Always 0 (success)
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
