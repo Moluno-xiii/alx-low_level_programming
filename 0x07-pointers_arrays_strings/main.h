37a6be912d6f865a1424dcc7384f81503354b0cd
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-09-25 07:13:41 -0700

    task 6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char *c);
+int _putchar(char c);
 char *_memcpy(char *det, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
