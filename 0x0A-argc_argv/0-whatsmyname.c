759caec54dbd415653628de388e1d8bc8b205219
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-10-02 04:32:31 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Function prints name of the program
+ * main - prints argument count
  * @argc: argument count
  * @argv: argument vector
+ *
  * Return: Always 0 (Success)
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
