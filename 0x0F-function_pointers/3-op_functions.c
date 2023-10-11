c4eb4895e0b643cf8cc6e21df470a364c1836222
Moluno Chukwuemeke Progress <molunoprogress@gmail.com>
2023-10-11 02:51:26 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function to sum two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Sum of two integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Function that subtracts two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Subtraction of two integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Function that  multiplies two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Multipication of two integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Function that divides two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Division of integers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Function finds the modulus of two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: Modulus of integers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
