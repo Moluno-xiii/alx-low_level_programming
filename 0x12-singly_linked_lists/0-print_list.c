665ecd3fefd60f7e6a517b0919c7b4a6662f7dc5
moluno_xiii <molunoprogress@gmail.com>
2023-10-19 16:04:15 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - Function prints all the elements of a list_t list
+ * @h: head
+ *
+ * Return: Always 0 (success)
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr;
+	size_t length = 0;
+
+	ptr = h;
+
+	while (ptr)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", ptr->length, ptr->str);
+		ptr = ptr->next;
+		length++;
+	}
+	return (length);
+}
+
