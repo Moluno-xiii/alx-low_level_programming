87eaea602c23c58cd27e697dc5f727c180efd337
moluno_xiii <molunoprogress@gmail.com>
2023-11-02 17:05:13 -0700

    task 0 done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,10 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include<sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/stat.>
+#include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
